70491640673f7e0507c56313626b8efae67924e7
wanderlee <wanderleedev@gmail.com>
2024-03-22 21:59:31 -0500

    feat Angular 17 template

---
+++ Plantilla-base-angular17--my-config/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig } from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
+    provideHttpClient(withFetch()),
+  ],
+};
