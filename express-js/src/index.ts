0bd131ff1bdc5d435b5571cc536a1d6bd14816df
wanderlee <wanderleedev@gmail.com>
2024-09-26 13:23:41 -0500

    feat: :heavy_plus_sign: add morgan types

--- express-js/src/index.ts
+++ express-js/src/index.ts
@@ -1,16 +1,20 @@
 import express from "express";
-import swaggerUi from "swagger-ui-express"
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 import specs from "./docs/swagger";
 
 const app = express();
 const PORT = 3000;
 
 // Middleware para manejar datos JSON
-app.use(express.json())
+app.use(express.json());
+
+// Middleware para registrar solicitudes
+app.use(morgan("dev"));
 
 // Integrar Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   console.log(`Server listen: http://localhost:${PORT}`);
-})
+});
